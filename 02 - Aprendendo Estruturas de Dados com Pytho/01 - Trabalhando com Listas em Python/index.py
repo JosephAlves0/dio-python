f22227eff85738f5433fe6d993dcda69f0eaf80c
JosephAlves0 <j.eduardo.alves@hotmail.com>
2024-06-19 20:33:04 -0300

    Módulo finalizado: Trabalhando com listas

---
+++ 02 - Aprendendo Estruturas de Dados com Pytho/01 - Trabalhando com Listas em Python/index.py
@@ -0,0 +1,1 @@
+linguagens = ["python", "js", "c"];
---
+++ 02 - Aprendendo Estruturas de Dados com Pytho/01 - Trabalhando com Listas em Python/quiz.py
@@ -0,0 +1,5 @@
+frutas = ["maçã", "laranja", "uva", "pera"];
+
+print(frutas[0]);
+
+print([n**2 if n > 6 else n for n in range(10) if n % 2 == 0])
